a6afce046a453a51447bdecc7979c8cc046a444a
Ayaj Husain <149255327+AyajHusain@users.noreply.github.com>
2024-01-24 14:11:25 +0530

    production build for frontend

--- index.js
+++ index.js
@@ -101,6 +101,6 @@ const unknownEndpoint = (request,response,next) =>{
 app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${3001}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
